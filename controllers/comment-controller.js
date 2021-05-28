fd3cbc52006eddc4064e04a1546af8c6aa3f364c
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-05-27 17:36:14 -0700

    comment controller set up

---
+++ controllers/comment-controller.js
@@ -0,0 +1,15 @@
+const { Comment, Pizza } = require('../models');
+const { modelName } = require('../models/Pizza');
+
+const commentController = {
+    //add comment to a pizza
+    addComment() {
+
+    },
+    //remove comment
+    removeComment() {
+
+    }
+};
+
+module.exports = commentController;
