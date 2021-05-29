ca59172ac78ff57eeb8630113fbef4da07884e47
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-05-28 21:20:33 -0700

    updates

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,14 +1,56 @@
 const { Comment, Pizza } = require('../models');
-const { modelName } = require('../models/Pizza');
+
 
 const commentController = {
     //add comment to a pizza
-    addComment() {
+    // async addComment({ params, body }, res) {
+    //     console.log(body);
+    //     const newComment = await Comment.create(body);
+    //     const { _id } = params;
+    //     console.log(_id);
+    // },
 
+
+    addComment({ params, body }, res) {
+        console.log(body);
+        Comment.create(body)
+            .then(({ _id }) => {
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    { $push: { comments: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'The Pizza was not found' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
     },
     //remove comment
-    removeComment() {
-
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({ _id: params.commentId })
+            .then(deletedComment => {
+                if (!deletedComment) {
+                    return res.status(404).json({ message: 'No comment with this id!' });
+                }
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    { $pull: { comments: params.commentId } },
+                    { new: true }
+                );
+            })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
     }
 };
 
